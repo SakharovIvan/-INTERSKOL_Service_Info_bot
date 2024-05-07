a4e317e3b0904bfd843d8137a9bcdd23bebb1bcb
i.sakharov <i731901@gmail.com>
2024-05-07 22:06:37 +0300

    8

--- SPmodel.js
+++ SPmodel.js
@@ -2,8 +2,8 @@ const { DataTypes } = require('sequelize');
 const sequelizeSP = require('./SpareParts_bd')
 
 const SparePart = sequelizeSP.define('sparePart',{
-   // id:{type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
-    sp:{type: DataTypes.STRING,primaryKey: true,unique: true},
+    id:{type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
+    sp:{type: DataTypes.STRING,primaryKey: true},
     name:{type:DataTypes.STRING},
     tools:{type:DataTypes.ARRAY(DataTypes.STRING)}
 })
