8beebb2d11f64bf6b1201d1157e847bb4651d4ce
i.sakharov <i731901@gmail.com>
2024-05-08 23:38:22 +0300

    1

--- file_bd.js
+++ file_bd.js
@@ -13,82 +13,81 @@ const client = new Client  ({
     port:'5432',
 })
 
-const querry = `
+const createTable = `
 CREATE TABLE SparePartmas ( 
     id bigint GENERATED ALWAYS AS IDENTITY,
     sp VARCHAR(50),
     name VARCHAR(150),
     tools text
     );
-    INSERT INTO SparePartmas (sp,name,tools)
-VALUES ('hello','im warking','qwerty');
-SELECT * FROM SparePartmas;
 `
+const makeRow = (table, tableRow, row)=>{
+const pgRow = `INSERT INTO ${table} (${tableRow}) VALUES ${row};`
+return pgRow
+}
 
-client.connect((err)=>{
-client.query(querry,(err,res)=>{
-    console.log(err?err.stack:res.rows[0])
-    client.end
-})
-})
+const deleteTable = `DROP TABLE SparePartmas;`
 
-
-
-//try {     
-//    const res = await client.query(querry); 
-//    console.log('Table is successfully created', res.rows[0]); 
-//    } catch (err) { 
-//    console.log(err); 
-//    } finally { 
-//    client.close(); 
-//    } 
-
-
-
-
-//
-//const createTable = async()=>{
-//    try { 
-//        await client.query(query); 
-//        return console.log('Table is successfully created'); 
-//        } catch (err) { 
-//        console.log(err); 
-//        } finally { 
-//        client.close(); 
-//        } 
-//}
-//
-//const RE_EOL = /\r?\n/;
-//const TAB =/\t/
-//class SP  {
-//    constructor([tool,sp,name]){
-//        this.tool=[tool]
-//        this.sp =sp
-//        this.name=name
-//    }
-//    addTool(newtool) {
-//        this.tool.push(newtool)
-//    }
-//}
-//
-//
-//const makeFromFileBD = async ()=> {
-//const readFile = util.promisify(fs.readFile)
-//const fileData = await readFile('./SP-ToolsUTF-8.txt', 'utf-8')
-//const masData = fileData.split(RE_EOL)
-//const masSP=[]
-//const promises = masData.map((id)=>{
-//    let sp_info = new SP(id.split(TAB))
-//    masSP.forEach((id)=>{
-//        if (id.sp==sp_info.sp){
-//            id.addTool(sp_info.tool[0])
-//        }
-//    })
-//    masSP.push(sp_info)
-//
+//client.connect((err)=>{
+//client.query(querry,(err,res)=>{
+//    console.log(err?err.stack:res.rows[0])
+//    client.end
 //})
-//await Promise.all(promises)
-//return masSP
-//}
-//
-//
+//})
+
+const RE_EOL = /\r?\n/;
+const TAB =/\t/
+
+class SP  {
+    constructor([tool,sp,name]){
+        this.tool=[tool]
+        this.sp =sp
+        this.name=name
+    }
+    addTool(newtool) {
+        this.tool.push(newtool)
+    }
+}
+
+
+const makeFromFileSparePartArray = async ()=> {
+const readFile = util.promisify(fs.readFile)
+const fileData = await readFile('./SP-ToolsUTF-8.txt', 'utf-8')
+const masData = fileData.split(RE_EOL)
+const masSP=[]
+const promises = masData.map((id)=>{
+    let sp_info = new SP(id.split(TAB))
+    masSP.forEach((id)=>{
+        if (id.sp==sp_info.sp){
+            id.addTool(sp_info.tool[0])
+        }
+    })
+    masSP.push(sp_info)
+
+})
+await Promise.all(promises)
+return masSP
+}
+
+const start = async()=>{
+    const massp = await makeFromFileSparePartArray()
+    console.log(massp[2])
+    const table = 'SparePartmas'
+    const tableRow = 'sp, name, tools'
+    let row
+    try{
+    await client.query(deleteTable)
+    await client.query(createTable)
+    console.log('Old table deleted, new created')
+}catch(err){
+    console.log('Cant delete|create table',err)
+}
+
+for (let sp_info of massp){
+    row = `${sp_info["sp"]}, ${sp_info["name"]}, ${sp_info["tools"]},`
+    client.query(makeRow(table,tableRow,row))
+}
+
+
+}
+start()
