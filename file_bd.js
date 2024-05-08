3b57d9e79ab8f3b40dae935736ae6222b5109202
i.sakharov <i731901@gmail.com>
2024-05-08 22:55:14 +0300

    1

--- file_bd.js
+++ file_bd.js
@@ -1,7 +1,7 @@
-const fs = require('fs')
-const util = require('util') 
+//const fs = require('fs')
+//const util = require('util') 
 const {Client} = require('pg');
-//const { database, password } = require('pg/lib/defaults');
+
 
 const client = new Client  ({
     user:'root',
@@ -12,57 +12,13 @@ const client = new Client  ({
 })
 
 const query = `
-CREATE TABLE SparePart ( 
+CREATE TABLE SparePartmas ( 
     id bigint GENERATED ALWAYS AS IDENTITY,
     sp VARCHAR(50),
     name VARCHAR(150),
     tools text
     );
 `
-
-const createTable = async()=>{
-    try { 
-        await client.query(query); 
-        return console.log('Table is successfully created'); 
-        } catch (err) { 
-        console.log(err); 
-        } finally { 
-        client.close(); 
-        } 
-}
-
-const RE_EOL = /\r?\n/;
-const TAB =/\t/
-class SP  {
-    constructor([tool,sp,name]){
-        this.tool=[tool]
-        this.sp =sp
-        this.name=name
-    }
-    addTool(newtool) {
-        this.tool.push(newtool)
-    }
-}
-
-
-const makeFromFileBD = async ()=> {
-const readFile = util.promisify(fs.readFile)
-const fileData = await readFile('./SP-ToolsUTF-8.txt', 'utf-8')
-const masData = fileData.split(RE_EOL)
-const masSP=[]
-const promises = masData.map((id)=>{
-    let sp_info = new SP(id.split(TAB))
-    masSP.forEach((id)=>{
-        if (id.sp==sp_info.sp){
-            id.addTool(sp_info.tool[0])
-        }
-    })
-    masSP.push(sp_info)
-
-})
-await Promise.all(promises)
-return masSP
-}
 
 client.query(query, (err, res) => { 
     if (err) { 
@@ -72,3 +28,53 @@ client.query(query, (err, res) => {
     console.log('Table is successfully created'); 
     client.end(); 
     }); 
+
+
+
+
+//
+//const createTable = async()=>{
+//    try { 
+//        await client.query(query); 
+//        return console.log('Table is successfully created'); 
+//        } catch (err) { 
+//        console.log(err); 
+//        } finally { 
+//        client.close(); 
+//        } 
+//}
+//
+//const RE_EOL = /\r?\n/;
+//const TAB =/\t/
+//class SP  {
+//    constructor([tool,sp,name]){
+//        this.tool=[tool]
+//        this.sp =sp
+//        this.name=name
+//    }
+//    addTool(newtool) {
+//        this.tool.push(newtool)
+//    }
+//}
+//
+//
+//const makeFromFileBD = async ()=> {
+//const readFile = util.promisify(fs.readFile)
+//const fileData = await readFile('./SP-ToolsUTF-8.txt', 'utf-8')
+//const masData = fileData.split(RE_EOL)
+//const masSP=[]
+//const promises = masData.map((id)=>{
+//    let sp_info = new SP(id.split(TAB))
+//    masSP.forEach((id)=>{
+//        if (id.sp==sp_info.sp){
+//            id.addTool(sp_info.tool[0])
+//        }
+//    })
+//    masSP.push(sp_info)
+//
+//})
+//await Promise.all(promises)
+//return masSP
+//}
+//
+//
