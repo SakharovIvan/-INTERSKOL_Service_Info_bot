93caa0d4b5199f9d3d06651a635ca2a9ab3fb072
i.sakharov <i731901@gmail.com>
2024-05-11 14:44:10 +0300

    tool_cards added2

--- index.js
+++ index.js
@@ -7,68 +7,61 @@ const replaceAll=(str, find, replace)=>{
     return str.replace(new RegExp(find, 'g'), replace);
   }
 
-const spCheck = async(chatID)=>{
-    bot.on('callback_query', async msg=>{
-        const text = msg.text
-        console.log(text)
-//        const chatID = msg.chat.id
-        try{
+  chats = {}
+const spCheck = async(text)=>{
         const info = await findMatNoSP(text)
-        console.log(info)
-        //if (info===undefined){return await bot.sendMessage(chatID, `Информация по артикулу не найден`)}
         const spToolsInfo = replaceAll(info['tools'],',','\n')
         const spMessage = `${info['sp']}\n${info['name']}\nСписок инструментов:\n${spToolsInfo} `
-        await bot.sendMessage(chatID, `Найдена следующая информация\n ${spMessage}`)
-    }catch(err){
-        console.log(err)
-    }
-    console.log('Вышлти из ботон спчек')
-    await bot.sendMessage(chatID, `Для поиска другого артикула, введите команду \`/sp_info\``)
-        return 
-    })
-    
-return 
+        console.log('Вышлти из спчека спчек')
+        return spMessage
 }
 
+const start = async () => {
+  bot.setMyCommands([
+    { command: "/start", description: "Начальное приветствие" },
+    { command: "/info", description: "Информация о клиенте" },
+    // {command: '/tool_info', description: 'Информация об инструменте'},
+    { command: "/sp_info", description: "Поиск инструмента по запчасти" },
+  ]);
 
-const start =async()=>{
-bot.setMyCommands ([
-    {command: '/start', description: 'Начальное приветствие'},
-    {command: '/info', description: 'Информация о клиенте'},
-   // {command: '/tool_info', description: 'Информация об инструменте'},
-    {command: '/sp_info', description: 'Поиск инструмента по запчасти'}
-])
-
-bot.on('message', async msg =>{
-    const text = msg.text
-    const chatID = msg.chat.id
-    try{
-    if (text === '/start'){
-        await bot.sendPhoto(chatID, './INTERSKOL_logo.jpg')
-        await bot.sendMessage(chatID, `Добро пожаловать в телграм бот по информационной системе ИНТЕРСКОЛ`)
-        return
-    }
-    if (text === '/info'){
-        return bot.sendMessage(chatID, `Для поиска применимости запчасти введите команду \`/sp_info\` и введите артикул ЗЧ\n
-        Для поиска вхем инструмента введите команду \`/tool_info\` и введите код инструмента - первые числа до точки в серийном номере нструмента или артикула с коробки инструмента`)
-    }
-    if (text === '/sp_info'){
+  bot.on("message", async (msg) => {
+    const text = msg.text;
+    const chatID = msg.chat.id;
+    try {
+      if (text === "/start") {
+        await bot.sendPhoto(chatID, "./INTERSKOL_logo.jpg");
+        await bot.sendMessage(
+          chatID,
+          `Добро пожаловать в телграм бот по информационной системе ИНТЕРСКОЛ`
+        );
+        return;
+      }
+      if (text === "/info") {
+        return bot.sendMessage(
+          chatID,
+          `Для поиска применимости запчасти введите команду \`/sp_info\` и введите артикул ЗЧ\n
+        Для поиска вхем инструмента введите команду \`/tool_info\` и введите код инструмента - первые числа до точки в серийном номере нструмента или артикула с коробки инструмента`
+        );
+      }
+      if (text === "/sp_info") {
         bot.sendMessage(chatID, `Ведите артикул запчасти`)
-        .then(()=>spCheck(chatID))
-        .then(()=>console.log('Я не буду работать1'))
-        .then(()=>{return})
-        .catch((err)=>console.log(err))
-        console.log('Я не буду работать2')
-        
+        return 
+      }
+      return bot.sendMessage(chatID, `Я тебя не понимаю`);
+    } catch (err) {
+      console.log(err);
     }
-    return bot.sendMessage(chatID, `Я тебя не понимаю`)
+  });
 
-}catch(err){ console.log(err)}
 
-} 
-)
-
-}
+  bot.on('callback_query', async msg=>{
+    const text = msg.text;
+    const chatID = msg.chat.id;
+    const answer = spCheck(text)
+    bot.sendMessage(chatID,answer)
+   
+  })
+};
 
 start()
 
