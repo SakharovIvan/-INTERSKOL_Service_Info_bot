fca7d1181a8d2ca36a2ec51cf56a5601e6f7f8bb
i.sakharov <i731901@gmail.com>
2024-05-11 19:56:30 +0300

    tool_cards added2

--- sp_find.js
+++ sp_find.js
@@ -2,8 +2,6 @@
 const { Pool } = require("pg");
 
 
-const sqlFilter =(table, param) => `SELECT * FROM ${table} WHERE ${param};`
-
 const client = new Pool({
     user: "root",
     host: "192.168.0.74",
@@ -17,11 +15,11 @@ const findMatNoSP = async (matNoSp) =>{
    // const table = "sparepartmas";
 
     try{
-        const pool = await client.connect()
-        console.log('CLient connected')
-        let result = await client.query(`SELECT * FROM sparepartmas WHERE sp = '${matNoSp}';`)
+        //const pool = await client.connect()
+        //console.log('CLient connected')
+        let result = await client.query(`SELECT * FROM sparepartmas WHERE sp = $1;`,)
         console.log('Filtered table', result.rows[0])
-        pool.end().then(()=>console.log('Client disconected'))
+       // pool.end().then(()=>console.log('Client disconected'))
         return result.rows[0]
         //console.log(result)
     }catch(err){
