8ec79e7043cc98efca1b9993c0153b2115671ea7
i.sakharov <i731901@gmail.com>
2024-05-11 16:36:48 +0300

    tool_cards added2

--- sp_find.js
+++ sp_find.js
@@ -10,6 +10,7 @@ const client = new Pool({
     database: "SpareParts_bd",
     password: "root",
     port: "5432",
+    connectionTimeoutMillis: 100,
   });
   
 const findMatNoSP = async (matNoSp) =>{
