015ea6d0fc5e7f00274fb09973fe048a911b1bf7
i.sakharov <i731901@gmail.com>
2024-05-15 20:14:09 +0300

    refactoring 150524

--- log/log_add.js
+++ log/log_add.js
@@ -1,9 +1,10 @@
 const {pool} = require('./log_bd.js')
 
-export const logADD = async(chatID, clie, text, time)=>{
+ const logADD = async(chatID, clie, text, time)=>{
 try{
     await pool.query(`INSERT INTO clientlog (chatid, cli, text,time) VALUES (${chatID},'${clie}','${text}',${time} );`)
     console.log('Cli Log added')
 }catch(err){console.log(err)}
 }
 
+module.exports = logADD
