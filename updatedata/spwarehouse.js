be4c946ae58cdb8f140b539806600cb3020a8284
i.sakharov <i731901@gmail.com>
2024-05-19 15:20:04 +0300

    3

--- updatedata/spwarehouse.js
+++ updatedata/spwarehouse.js
@@ -7,16 +7,18 @@ const RE_EOL = /\r?\n/;
 const TAB = /\t/;
 
 const updateSQL = (table, value, sp,date) =>{
-    return `UPDATE ${table} SET warehouse = '${value}' warehousedateupdate = '${date}'  WHERE sp = '${sp}';`
+    return `UPDATE ${table} SET warehouse = '${value}' warehousedateupdate = TO_DATE('${date}')  WHERE sp = '${sp}';`
 }
 
 const warehouseDataAddtoSQL = async()=>{
     const readFile = util.promisify(fs.readFile);
+    const currentdate = new Date()
+    const curdate = `${currentdate.getFullYear()}-${currentdate.getMonth()+1}-${currentdate.getDate()}`
+    console.log(curdate)
     try{
     const fileData = await readFile(pathSP_warehouse, "utf-8");
     const masData = await fileData.split(RE_EOL);
-    const currentdate = new Date()
-    const curdate = `${currentdate.getFullYear()}-${currentdate.getMonth()+1}-${currentdate.getDate()}`
+    
     await client.connect()
     for (let spwarestatus of masData){
         const spwarestatusrow = await spwarestatus.split(TAB)
