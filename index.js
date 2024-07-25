ca9d813f84645d251e4fbd1661948c5fdd20a8e0
i.sakharov <i731901@gmail.com>
2024-07-25 16:09:33 +0300

    node

--- index.js
+++ index.js
@@ -1,55 +1,59 @@
-const TelegramAPI = require('node-telegram-bot-api')
-const {token} = require("./db.js")
-const bot = new TelegramAPI(token,{polling:true})
-const logADD = require('./log/log_add.js')
+const TelegramAPI = require("node-telegram-bot-api");
+const { token } = require("./db.js");
+const bot = new TelegramAPI(token, { polling: true });
+const logADD = require("./log/log_add.js");
 const fs = require("file-system");
-const { toolFilter, findMatNoSP, findSPbyChar} = require('./SQLtablefilters.js')
-const {toolspcardsupload} = require('./updatedata/toolspcardsupload.js')
-const logo = './data/INTERSKOL_logo.jpg'
-const {update_sp_data} = require('./updatedata/file_bd.js')
-const {warehouseDataAddtoSQL} = require('./updatedata/spwarehouse.js')
-const {write_files_to_SQL}=require('./updatedata/tool_cards_sql.js')
+const {
+  toolFilter,
+  findMatNoSP,
+  findSPbyChar,
+} = require("./SQLtablefilters.js");
+const { toolspcardsupload } = require("./updatedata/toolspcardsupload.js");
+const logo = "./data/INTERSKOL_logo.jpg";
+const { update_sp_data } = require("./updatedata/file_bd.js");
+const { warehouseDataAddtoSQL } = require("./updatedata/spwarehouse.js");
+const { write_files_to_SQL } = require("./updatedata/tool_cards_sql.js");
 
-const pathSP_tools='./data/pathSP_tools.txt'
-const pathSP_warehouse='./data/pathSP_warehouse.txt'
+const pathSP_tools = "./data/pathSP_tools.txt";
+const pathSP_warehouse = "./data/pathSP_warehouse.txt";
 
 const WAY2 = /.+\(/;
 const CODEDEL = /\)/;
 
-const spCheck = async(chatID, text)=>{
-
- try{
-    const info = await findMatNoSP(text)
-    let toolsInlineKeyboar = []
-    const spToolsArray = await info['tools'].split(',')
-    for (let toolArr of spToolsArray){
-      let toolNumber = await toolArr.replace(WAY2, "").replace(CODEDEL, "")
-      toolsInlineKeyboar.push([{text: toolArr, callback_data: toolNumber}])
+const spCheck = async (chatID, text) => {
+  try {
+    const info = await findMatNoSP(text);
+    let toolsInlineKeyboar = [];
+    const spToolsArray = await info["tools"].split(",");
+    for (let toolArr of spToolsArray) {
+      let toolNumber = await toolArr.replace(WAY2, "").replace(CODEDEL, "");
+      toolsInlineKeyboar.push([{ text: toolArr, callback_data: toolNumber }]);
     }
-    const spMessage = await `${info['sp']}\n${info['name']}\n${info['characteristics']}\nСклад '${info['warehouse']}' \nОбновление от ${ info['warehousedateupdate']}`
-    await bot.sendMessage(chatID,`Вот что нашел:\n${spMessage}`)
-    await bot.sendMessage(chatID, 'Вы можете выбрать инструмент',{
+    const spMessage =
+      await `${info["sp"]}\n${info["name"]}\n${info["characteristics"]}\nСклад '${info["warehouse"]}' \nОбновление от ${info["warehousedateupdate"]}`;
+    await bot.sendMessage(chatID, `Вот что нашел:\n${spMessage}`);
+    await bot.sendMessage(chatID, "Вы можете выбрать инструмент", {
       reply_markup: {
-        inline_keyboard: toolsInlineKeyboar
-      }
-    })
-    return 
- }catch(err){
-    console.log('ПРоблема с поиском ЗЧ ',err)
- }
- try{
-  const info = await toolFilter(text.toUpperCase())
-  for (let el of info){
-    bot.sendMessage(chatID,`Вот что нашел:`)
-    await bot.sendMessage(chatID, el['toolname'])
-    await bot.sendDocument(chatID, el['toolschemedir'])
+        inline_keyboard: toolsInlineKeyboar,
+      },
+    });
+    return;
+  } catch (err) {
+    console.log("ПРоблема с поиском ЗЧ ", err);
   }
-return
-}catch(err){
-  console.log('ПРоблема с поиском инструмента ',err)
-}
-return bot.sendMessage(chatID,`Такого я не нашел(((`)
-}
+  try {
+    const info = await toolFilter(text.toUpperCase());
+    for (let el of info) {
+      bot.sendMessage(chatID, `Вот что нашел:`);
+      await bot.sendMessage(chatID, el["toolname"]);
+      await bot.sendDocument(chatID, el["toolschemedir"]);
+    }
+    return;
+  } catch (err) {
+    console.log("ПРоблема с поиском инструмента ", err);
+  }
+  return bot.sendMessage(chatID, `Такого я не нашел(((`);
+};
 
 const start = async () => {
   console.log("start");
@@ -65,7 +69,6 @@ const start = async () => {
     try {
       await logADD(chatID, username, text, time);
       switch (text) {
-        
         case "/start":
           const INterlogo = fs.readFileSync(logo);
           await bot.sendPhoto(chatID, INterlogo);
@@ -89,10 +92,18 @@ const start = async () => {
         case "/updatedata":
           console.log("прошли проверка");
           try {
-            await update_sp_data();
-            await bot.sendMessage(chatID, "Данные по привязкам ЗЧ к инстурменту обновлены");
-            await write_files_to_SQL();
-            await bot.sendMessage(chatID, "Данные по взрывсхемам обновлены");
+            update_sp_data()
+              .then(() => {
+                bot.sendMessage(
+                  chatID,
+                  "Данные по привязкам ЗЧ к инстурменту обновлены"
+                );
+              })
+              .then(() => {
+                write_files_to_SQL().then(() => {
+                  bot.sendMessage(chatID, "Данные по взрывсхемам обновлены");
+                });
+              });
           } catch (err) {
             console.log(err);
             await bot.sendMessage(chatID, "Произошла ошибка", err);
@@ -107,20 +118,25 @@ const start = async () => {
 
         if (msg.document.file_name === "uploadtoolspcards.txt") {
           try {
-            await bot.sendMessage(chatID, "Началась загрузка файла файла со списокм ЗЧ с инструментом");
+            await bot.sendMessage(
+              chatID,
+              "Началась загрузка файла файла со списокм ЗЧ с инструментом"
+            );
             const thumbPath = await bot.getFileLink(msg.document.file_id);
             await bot.sendMessage(chatID, thumbPath);
             await toolspcardsupload(thumbPath, pathSP_tools);
             await bot.sendMessage(chatID, "Файл загружен успешно");
           } catch (err) {
             await bot.sendMessage(chatID, "Произошла ошибка", err);
-            await logADD(chatID, username, text, time, err);
             console.log(err);
           }
         }
         if (msg.document.file_name === "spwarehouse.txt") {
           try {
-            await bot.sendMessage(chatID, "Началась загрузка файоа файла со складом ЗЧ");
+            await bot.sendMessage(
+              chatID,
+              "Началась загрузка файоа файла со складом ЗЧ"
+            );
             const thumbPath = await bot.getFileLink(msg.document.file_id);
             await bot.sendMessage(chatID, thumbPath);
             await toolspcardsupload(thumbPath, pathSP_warehouse);
@@ -132,7 +148,7 @@ const start = async () => {
         }
       }
 
-      return; 
+      return;
     } catch (err) {
       console.log("проблема с обработкой сообщения", err, msg);
     }
@@ -148,4 +164,3 @@ const start = async () => {
 };
 
 start();
-
